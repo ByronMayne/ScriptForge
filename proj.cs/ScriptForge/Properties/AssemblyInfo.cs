8a3b674fe3fe300e79693cf1178ca983c7049a05
Byron Mayne <ByronMayne@gmail.com>
2016-10-01 20:56:35 -0400

    Auto ignore anything with the name Test
    Removed all Unity cs proj from being comitted.

---
+++ proj.cs/ScriptForge/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScriptForge")]
+[assembly: AssemblyCompany("Byron Mayne")]
+[assembly: AssemblyProduct("ScriptForge")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f99c625a-5e70-4927-aea4-06aaf9076d68")]
+[assembly: AssemblyIsEditorAssembly]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
