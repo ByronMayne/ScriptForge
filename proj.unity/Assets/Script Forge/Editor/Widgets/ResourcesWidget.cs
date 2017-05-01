eabb9e26306d65687b68e018ceb652b900533c9a
Byron Mayne <ByronMayne@gmail.com>
2017-05-01 09:23:52 -0400

    Started working on the Resources Widget.

--- proj.unity/Assets/Script Forge/Editor/Widgets/ResourcesWidget.cs
+++ proj.unity/Assets/Script Forge/Editor/Widgets/ResourcesWidget.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
 using System.Collections.Generic;
-using System.IO;
+using UnityEditor;
+using UnityEditorInternal;
+using System;
 
 namespace ScriptForge
 {
     [System.Serializable]
     public class ResourcesWidget : ForgeWidget
     {
+        private const float BUTTON_WIDTH = 40;
+        private GUIContent m_HeaderLabel = new GUIContent("Included Resource Folders");
+
+        [SerializeField]
+        protected List<string> m_FoldersToInclude;
+
+        protected ReorderableList m_SerilaizedList;
+
         public override GUIContent label
         {
             get
@@ -32,7 +40,7 @@ namespace ScriptForge
         {
             get
             {
-                return LayoutSettings.ANIMATION_WIDGET_SORT_ORDER;
+                return LayoutSettings.RESOURCES_WIDGET_SORT_ORDER;
             }
         }
 
@@ -43,8 +51,37 @@ namespace ScriptForge
         {
             get
             {
-                return "Tags";
+                return "ResourcePaths";
             }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            // Create our list. 
+            m_SerilaizedList = new ReorderableList(m_FoldersToInclude, typeof(string));
+            m_SerilaizedList.drawElementCallback += OnDrawElement;
+            m_SerilaizedList.drawHeaderCallback += OnDrawHeader;
+            m_SerilaizedList.onAddCallback += OnFolderAdded;
+        }
+
+
+        /// <summary>
+        /// Invoked when we are drawing our header.
+        /// </summary>
+        private void OnDrawHeader(Rect rect)
+        {
+            GUI.Label(rect, m_HeaderLabel);
+        }
+
+        /// <summary>
+        /// Invoked when we need to draw our element 
+        /// </summary>
+        private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            Rect contentRect = rect;
+            contentRect.width -= BUTTON_WIDTH;
+            GUI.Label(contentRect, m_FoldersToInclude[index]); 
         }
 
         /// <summary>
@@ -53,6 +90,25 @@ namespace ScriptForge
         protected override void DrawWidgetContent(ScriptForgeStyles style)
         {
             base.DrawWidgetContent(style);
+            m_SerilaizedList.DoLayoutList();
+        }
+
+        /// <summary>
+        /// Invoked when we add a new element to our list. 
+        /// </summary>
+        private void OnFolderAdded(ReorderableList list)
+        {
+            // Get the path 
+            string path = EditorUtility.OpenFolderPanel("Folder", Application.dataPath, "Resources");
+            // Convert it to a unity path
+            path = FileUtil.GetProjectRelativePath(path);
+            // Add it
+            m_FoldersToInclude.Add(path);
+        }
+
+        protected string[] GetResourceAssetGUIDs()
+        {
+            return AssetDatabase.FindAssets("", m_FoldersToInclude.ToArray());
         }
 
         /// <summary>
@@ -60,8 +116,14 @@ namespace ScriptForge
         /// our hash with.
         protected override string GetHashInputString()
         {
-            return this.name;
-
+            string hashInput = string.Empty;
+            hashInput += m_Namespace;
+            hashInput += m_ClassName;
+            foreach (var assetPath in GetResourceAssetGUIDs())
+            {
+                hashInput += assetPath;
+            }
+            return hashInput;
         }
 
         /// <summary>
@@ -69,7 +131,18 @@ namespace ScriptForge
         /// </summary>
         public override void OnGenerate()
         {
-          
+            List<string> result = new List<string>();
+            foreach(string guid in GetResourceAssetGUIDs())
+            {
+                // Convert to our asset path
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                // Add it 
+                if(!result.Contains(assetPath))
+                {
+                    result.Add(assetPath);
+                    Debug.Log("Adding: " + assetPath);
+                }
+            }
         }
     }
 }
